99f9c52ebbefa47b319b513ebd5904a7e87e3aa9
Eduard Casas <eduard.casas@foonkiemonkey.co.uk>
2026-08-03 20:40:19 -0500

    test: simplify mock barrel and remove custom FakeFirebaseFirestore

--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -1,1 +1,5 @@
-export 'mocks/mocks.dart';
+export '../features/auth/helpers/auth_mocks.dart';
+export '../features/booking/helpers/booking_mocks.dart';
+export '../features/events/helpers/events_mocks.dart';
+export 'mocks/core/core_mocks.dart';
+export 'mocks/external/firebase_mocks.dart';
--- test/helpers/mocks/mocks.dart
+++
@@ -1,5 +0,0 @@
-export '../../features/auth/helpers/auth_mocks.dart';
-export '../../features/booking/helpers/booking_mocks.dart';
-export '../../features/events/helpers/events_mocks.dart';
-export 'core/core_mocks.dart';
-export 'external/firebase_mocks.dart';
